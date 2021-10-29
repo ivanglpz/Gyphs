fe75d4df33add62986f9ff612c4300fe1c8d5941
Ivan <miltonivan117@gmail.com>
2021-10-29 14:23:44 -0600

    error context import

--- src/hooks/useContext.ts
+++ src/hooks/useContext.ts
@@ -1,8 +1,8 @@
 import { createContext, Dispatch, SetStateAction } from "react";
-import { IDetailGif } from "../pages/Search";
-
+// import { IDetailGif } from "../pages/Search";
+import { IDataGif } from "../types/types";
 interface IContext {
-  setDetailGif: Dispatch<SetStateAction<IDetailGif>>;
+  setDetailGif: Dispatch<SetStateAction<IDataGif>>;
 }
 
 const UserContext = createContext<IContext>({} as IContext);
