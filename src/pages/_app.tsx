670015e57fa34ec1cd1a970d3fed863735b43fe9
whil <miltonivan117@gmail.com>
2021-11-09 10:13:59 -0600

    useffect to layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,7 +26,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   const [userApp, setUserApp] = useState<IUser>({} as IUser);
   const router = useRouter();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setTheme(JSON.parse(Cookies.get("@theme") || "light"));
   }, []);
 
