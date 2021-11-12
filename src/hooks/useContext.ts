3467060b869a0284a4cf45aff770a89b0601c2c1
whil <miltonivan117@gmail.com>
2021-11-12 15:14:31 -0600

    usercontext tags

--- src/hooks/useContext.ts
+++ src/hooks/useContext.ts
@@ -2,7 +2,7 @@ import { createContext, Dispatch, SetStateAction } from "react";
 import { IFormGif } from "../types/types";
 interface IContext {
   setDetailGif: Dispatch<SetStateAction<IFormGif>>;
-  handleTags: (tag: string) => void;
+  handleTags?: (tag: string) => void;
 }
 
 const UserContext = createContext<IContext>({} as IContext);
